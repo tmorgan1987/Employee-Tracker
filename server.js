81a6fd43465bcc02f87e7f664e3ac202564369e6
tmorgan1987 <tmorgan1987@outlook.com>
2022-11-07 23:30:37 -0500

    Added an inquirer prompt.

---
+++ server.js
@@ -0,0 +1,35 @@
+// sets up basic requirements.
+const db = require('./db/connection'); 
+const inquirer = require('inquirer');
+
+//let's get a menu rolling.
+const menu = () => {
+	inquirer.prompt({ type: 'list', name: 'selection', message: 'What would you like to do?', choices: ['View Employees', 'View Employee Roles', 'View the Departments', 'Add an Employee', 'Update an Employee', 'Add an Employee Role', 'Add a Department', 'Quit'] })
+	.then(({ selection }) => {
+		if (selection === 'View Employees') {
+			viewEmployees();
+		} else if (selection === 'View Employee Roles') {
+			viewRoles();
+		} else if (selection === 'View the Departments') {
+			viewDepartments();
+		} else if (selection === 'Add an Employee') {
+			addEmployee();
+		} else if (selection === 'Update an Employee') {
+			updateEmployee();
+		} else if (selection === 'Add an Employee Role') {
+			addEmployeeRole();
+		} else if (selection === 'View the Departments') {
+			viewDepartments();
+		} else if (selection === 'Add an Employee') {
+			addEmployee();
+		} else if (selection === 'Update an Employee') {
+			updateEmployee();
+		} else if (selection === 'Add an Employee Role') {
+			addEmployeeRole();
+		} else if (selection === 'Add a Department') {
+			addDepartment();
+		} else (selection === 'Quit') {
+			process.exit();
+		};
+	});
+};
